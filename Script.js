da42d8fe5382a1b344119d05a1dd7ccfdfe9834e
nayakanjali588-commits <nayakanjali588@gmail.com>
2026-06-17 15:45:29 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,53 @@
+function screenCandidate(){
 
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let skills = document.getElementById("skills").value.toLowerCase();
+
+    if(name === "" || email === "" || skills === ""){
+        alert("Please fill all fields");
+        return;
+    }
+
+    let score = 0;
+
+    let requiredSkills = [
+        "python",
+        "java",
+        "html",
+        "css",
+        "javascript"
+    ];
+
+    requiredSkills.forEach(skill => {
+        if(skills.includes(skill)){
+            score += 20;
+        }
+    });
+
+    let status = "";
+
+    if(score >= 60){
+        status = "Selected";
+    }
+    else{
+        status = "Rejected";
+    }
+
+    document.getElementById("result").style.display = "block";
+
+    document.getElementById("candidateName").innerText = name;
+    document.getElementById("candidateEmail").innerText = email;
+    document.getElementById("score").innerText = score + "%";
+
+    let statusElement = document.getElementById("status");
+
+    statusElement.innerText = status;
+
+    if(status === "Selected"){
+        statusElement.className = "selected";
+    }
+    else{
+        statusElement.className = "rejected";
+    }
+}
